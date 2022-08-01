cf4c1ac1137e448183aa598071daf087dd2fca46
colonelblac <108042864+colonelblac@users.noreply.github.com>
2022-08-01 12:55:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -235,7 +235,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('‼️ JOIN OUR COMMUNITY ‼️', url='https://t.me/filmcognito') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
